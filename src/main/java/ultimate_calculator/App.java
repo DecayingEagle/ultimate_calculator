73682cb488ec9ebdd6363029ce9c0bda6132205f
Marcin Rozalski <therealeagle.yt@gmail.com>
2022-02-13 07:26:40 +0100

    init

---
+++ src/main/java/ultimate_calculator/App.java
@@ -0,0 +1,13 @@
+package ultimate_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
